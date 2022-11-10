4f9d6f1ecfffd31e11131294ba9462c04d7b3116
TravelFiend <mikegrace274@gmail.com>
2022-11-10 01:26:04 -0800

    added srollPosition hook

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useScrollPosition from '../hooks/scrollPosition';
+import PropTypes from 'prop-types';
 import HeroBanner from '../components/HeroBanner';
 import HeroImageRow from '../components/HeroImageRow';
 
 
 const Hero = () => {
+  const { yPos } = useScrollPosition();
+
+  useEffect(() => {
+    console.log({ yPos });
+  }, [yPos])
+
   return (
     <section className='section-hero'>
       <HeroBanner />
       <HeroImageRow />
     </section>
   )
+};
+
+Hero.propTypes = {
+  yPos: PropTypes.number.isRequired
 };
 
 export default Hero;
