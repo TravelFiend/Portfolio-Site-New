25ce885f7e95bd5ae3a964589e1039fa52b21c84
TravelFiend <mikegrace274@gmail.com>
2022-11-10 01:30:10 -0800

    removed unneeded useScrollPosition

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,27 +1,14 @@
-import React, { useEffect } from 'react';
-import useScrollPosition from '../hooks/scrollPosition';
-import PropTypes from 'prop-types';
+import React from 'react';
 import HeroBanner from '../components/HeroBanner';
 import HeroImageRow from '../components/HeroImageRow';
 
 
-const Hero = () => {
-  const { yPos } = useScrollPosition();
-
-  useEffect(() => {
-    console.log({ yPos });
-  }, [yPos])
-
-  return (
+const Hero = () => {return (
     <section className='section-hero'>
       <HeroBanner />
       <HeroImageRow />
     </section>
   )
-};
-
-Hero.propTypes = {
-  yPos: PropTypes.number.isRequired
 };
 
 export default Hero;
