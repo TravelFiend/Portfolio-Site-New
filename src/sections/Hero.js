f7e0b453330e58b54e2d3f69a7d64cd43c72e881
TravelFiend <mikegrace274@gmail.com>
2022-11-10 01:30:39 -0800

    removed unneeded useScrollPosition

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -3,7 +3,8 @@ import HeroBanner from '../components/HeroBanner';
 import HeroImageRow from '../components/HeroImageRow';
 
 
-const Hero = () => {return (
+const Hero = () => {
+  return (
     <section className='section-hero'>
       <HeroBanner />
       <HeroImageRow />
