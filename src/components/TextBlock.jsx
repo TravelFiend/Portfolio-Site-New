c6dbd799837ad797d97e1a57168ea0a50e5820a0
TravelFiend <mikegrace274@gmail.com>
2025-04-14 21:54:30 +0100

    about section done for desktop

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -2,9 +2,9 @@ const TextBlock = ({ header, description }) => {
   return (
     <div className='text-block' style={{ color: '#fff' }}>
       <h2>{header}</h2>
-      <p>{description}</p>
+      <p dangerouslySetInnerHTML={{ __html: description }} />
     </div>
-  )
-}
+  );
+};
 
 export default TextBlock;
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,9 +6,13 @@ import mikeCamel from '../../public/assets/mike-on-a-camel.jpg';
 
 
 const About = () => {
+  const textContent = {
+    header: 'Building Cool Stuff, One Line at a Time',
+    body: 'Hey, I\'m Mike Grace, a full-stack software engineer hooked on front-end magic. Since 2020, I\'ve built NextJS apps that flow, Shopify stores that pop, and Python scripts that save time. I\'ve helped launch mobile apps that clinched million-dollar deals and kept e-commerce projects rocking. Messy problems? I make them clean and simple.<br /><br />Off-duty, I\'m biking trails, fishing, or eating my way through new places. I can get by speaking German, love a good challenge, and pour heart into every project. Let\'s build something incredible together!'
+  }
   return (
     <section id='about' className='section-about'>
-      <TextBlock header='Mike Grace' description='This is me, Me am I.'/>
+      <TextBlock header={textContent.header} description={textContent.body} />
       <Composition pic1={mikeCamel} pic2={mikeMountain} pic3={mikeLaos} />
     </section>
   );
