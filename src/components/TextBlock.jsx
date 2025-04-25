fe4cf4b571868389c93fef0ad92712f6fdebbd0c
TravelFiend <mikegrace274@gmail.com>
2025-04-25 17:45:53 +0700

    breakpoints mixin created

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -2,7 +2,7 @@ const TextBlock = ({ header, description }) => {
   return (
     <div className='text-block' style={{ color: '#fff' }}>
       <h2>{header}</h2>
-      <p dangerouslySetInnerHTML={{ __html: description }} />
+      {description ? <p dangerouslySetInnerHTML={{ __html: description }} /> : null}
     </div>
   );
 };
