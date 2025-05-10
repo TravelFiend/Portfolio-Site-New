76ea32065057ef4e4d2bcebc0337de0a462fde3d
TravelFiend <mikegrace274@gmail.com>
2025-05-11 02:01:34 +0700

    work section almost done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,14 @@ const Footer = () => {
   return (
     <footer className='section-footer'>
       <SocialMenu />
+      <div id="stage">
+        <div id="traveler">
+          <div id="bouncer"></div>
+        </div>
+        <div id="traveler2">
+          <div id="bouncer2"></div>
+        </div>
+      </div>
     </footer>
   )
 }
