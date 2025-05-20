b322f29905a093ffe90ac65fe8c72d291e786992
TravelFiend <mikegrace274@gmail.com>
2025-05-20 15:59:00 +0700

    updated tech icons to use webp format instead of png

--- src/sections/Technologies.jsx
+++ src/sections/Technologies.jsx
@@ -1,25 +1,4 @@
 import useIntersectionObserver from '../hooks/useIntersectionObserver';
-import html5 from '../../public/assets/html5.png';
-import css from '../../public/assets/css.png';
-import js from '../../public/assets/js.png';
-import node from '../../public/assets/node.png';
-import express from '../../public/assets/express.png';
-import git from '../../public/assets/git.png';
-import github from '../../public/assets/github.png';
-import heroku from '../../public/assets/heroku.png';
-import jest from '../../public/assets/jest.png';
-import liquid from '../../public/assets/liquid.png';
-import mongodb from '../../public/assets/mongodb.png';
-import nextjs from '../../public/assets/nextjs.png';
-import postman from '../../public/assets/postman.png';
-import python from '../../public/assets/python.png';
-import qunit from '../../public/assets/qunit.png';
-import react from '../../public/assets/react.png';
-import redux from '../../public/assets/redux.png';
-import sanity from '../../public/assets/sanity.png';
-import shopify from '../../public/assets/shopify.png';
-import tailwind from '../../public/assets/tailwind.png';
-import travis from '../../public/assets/travis.png';
 import { useEffect, useRef, useState } from 'react';
 
 const Technologies = () => {
@@ -40,27 +19,27 @@ const Technologies = () => {
   }, [entry.isIntersecting, hasAnimated, setNode]);
 
   const techIcons = [
-    { icon: html5, text: 'HTML' },
-    { icon: css, text: 'CSS' },
-    { icon: js, text: 'Javascript' },
-    { icon: node, text: 'Node.js' },
-    { icon: express, text: 'Express.js' },
-    { icon: git, text: 'Git' },
-    { icon: github, text: 'Github' },
-    { icon: heroku, text: 'Heroku' },
-    { icon: jest, text: 'Jest' },
-    { icon: liquid, text: 'Liquid' },
-    { icon: mongodb, text: 'MongoDB' },
-    { icon: nextjs, text: 'NextJS' },
-    { icon: postman, text: 'Postman' },
-    { icon: python, text: 'Python' },
-    { icon: qunit, text: 'QUnit' },
-    { icon: react, text: 'React' },
-    { icon: redux, text: 'Redux' },
-    { icon: sanity, text: 'Sanity' },
-    { icon: shopify, text: 'Shopify' },
-    { icon: tailwind, text: 'Tailwind' },
-    { icon: travis,  text: 'Travis CI' }
+    { icon: 'html5', text: 'HTML' },
+    { icon: 'css', text: 'CSS' },
+    { icon: 'js', text: 'Javascript' },
+    { icon: 'node', text: 'Node.js' },
+    { icon: 'express', text: 'Express.js' },
+    { icon: 'git', text: 'Git' },
+    { icon: 'github', text: 'Github' },
+    { icon: 'heroku', text: 'Heroku' },
+    { icon: 'jest', text: 'Jest' },
+    { icon: 'liquid', text: 'Liquid' },
+    { icon: 'mongodb', text: 'MongoDB' },
+    { icon: 'nextjs', text: 'NextJS' },
+    { icon: 'postman', text: 'Postman' },
+    { icon: 'python', text: 'Python' },
+    { icon: 'qunit', text: 'QUnit' },
+    { icon: 'react', text: 'React' },
+    { icon: 'redux', text: 'Redux' },
+    { icon: 'sanity', text: 'Sanity' },
+    { icon: 'shopify', text: 'Shopify' },
+    { icon: 'tailwind', text: 'Tailwind' },
+    { icon: 'travis',  text: 'Travis CI' }
   ];
 
   return (
@@ -68,17 +47,17 @@ const Technologies = () => {
       {techIcons.map(({ icon, text }, index) => {
         return (
           <div key={index}>
-            {/* <picture style={{ animationDelay: `${index * .06}s` }}>
+            <picture>
               <source srcSet={`/assets/${icon}.webp`} type="image/webp" />
               <source srcSet={`/assets/${icon}.png`} type="image/png" />
-              <img src={`/assets/${icon}.png`} alt={`${text} icon`} loading="lazy" />
-            </picture> */}
-            <img
-              src={icon}
-              alt={`${text} icon`}
-              className={`${text === 'Tailwind' ? 'tailwindIcon' : ''}${text === 'Node.js' ? 'nodeIcon' : ''} ${hasAnimated ? 'animate' : ''}`}
-              style={{ animationDelay: `${index * .06}s` }}
-            />
+              <img
+                className={`${text === 'Tailwind' ? 'tailwindIcon' : ''}${text === 'Node.js' ? 'nodeIcon' : ''} ${hasAnimated ? 'animate' : ''}`}
+                style={{ animationDelay: `${index * .06}s` }}
+                src={`/assets/${icon}.png`}
+                alt={`${text} icon`}
+                loading="lazy"
+              />
+            </picture>
             <p
               className={hasAnimated ? 'animate' : ''}
               style={{ animationDelay: `${index * .06}s` }}
